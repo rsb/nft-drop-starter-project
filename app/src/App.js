f464049c5ad1464861121d205ce0159dab3470a5
Robert Scott-Buccleuch <60840+rsb@users.noreply.github.com>
2022-02-20 13:55:49 -0800

    connect your web app to solana

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
@@ -7,6 +7,8 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
   const checkIfWalletIsConnected = async () => {
     try {
       const { solana } = window;
@@ -19,10 +21,32 @@ const App = () => {
 
       const response = await solana.connect({ onlyIfTrusted: true });
       console.log('Connected with Public Key: ', response.publicKey.toString())
+      setWalletAddress(response.publicKey.toString())
     } catch (error) {
       console.log(error);
     }
   }
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (! solana) {
+      return
+    }
+
+    const response = await solana.connect();
+    console.log('Connected with Public Key: ', response.publicKey.toString())
+    setWalletAddress(response.publicKey.toString())
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className={'cta-button connect-wallet-button'}
+      onClick={connectWallet}
+      >
+        Connect to wallet
+    </button>
+  );
 
   useEffect(() => {
    const onLoad = async () => {
@@ -39,6 +63,7 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
